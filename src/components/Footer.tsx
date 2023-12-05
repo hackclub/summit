e84eb0419c57cf469aebe3c290a7f4b81fdd6c58
Faisal Sayed <faisal.sayed502@gmail.com>
2023-12-05 15:04:59 -0500

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-secondary py-5 text-center text-primary text-xl">
+      dream with us in #leaders-summit on Hack Club Slack
+    </div>
+  );
+};
+
+export default Footer;
