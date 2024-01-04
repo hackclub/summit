7f326b0239cffeec8d4450388177e6b682d85ff8
Faisal Sayed <faisal.sayed502@gmail.com>
2024-01-04 14:51:42 -0500

    removed website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,3 @@
-import FAQ from "@/components/FAQ";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Section2 from "@/components/Section2";
-import Signup from "@/components/Signup";
-import Story from "@/components/Story";
-import HeroMob from "@/components/HeroMob";
 import { Akaya_Kanadaka } from "next/font/google";
 
 const akaya = Akaya_Kanadaka({ weight: "400", subsets: ["latin"] });
@@ -13,16 +5,17 @@ const akaya = Akaya_Kanadaka({ weight: "400", subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={akaya.className + " relative"}>
-      <div className="absolute sm:right-5 right-2 h-full w-1 bg-secondary z-10" />
-      <div className="absolute sm:left-5 left-2 h-full w-1 bg-secondary z-10" />
+      what you seek is seeking you
+      {/* <div className="absolute sm:right-5 right-2 h-full w-1 bg-secondary z-10" />
+      <div className="absolute sm:left-5 left-2 h-full w-1 bg-secondary z-10" /> */}
       {/* <Navbar /> */}
-      <Hero />
+      {/* <Hero />
       <HeroMob />
       <Section2 />
       <Story />
       <Signup />
       <FAQ />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
