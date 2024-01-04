ddb5bdbcb983b0de82b15512c14182234878663e
Faisal Sayed <faisal.sayed502@gmail.com>
2024-01-04 15:27:12 -0500

    prank

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,71 @@
 import { Akaya_Kanadaka } from "next/font/google";
-
+import { useEffect, useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import YouTube from "react-youtube";
 const akaya = Akaya_Kanadaka({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
+  const [player, setPlayer] = useState<any>();
+  const cookieRef = useRef<HTMLDivElement>(null);
+  const videoRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (cookieRef.current) {
+      cookieRef.current.style.display = "block";
+    }
+  }, []);
+
   return (
-    <main className={akaya.className + " relative"}>
-      what you seek is seeking you
-      {/* <div className="absolute sm:right-5 right-2 h-full w-1 bg-secondary z-10" />
-      <div className="absolute sm:left-5 left-2 h-full w-1 bg-secondary z-10" /> */}
-      {/* <Navbar /> */}
-      {/* <Hero />
-      <HeroMob />
-      <Section2 />
-      <Story />
-      <Signup />
-      <FAQ />
-      <Footer /> */}
+    <main
+      className={
+        akaya.className + " relative flex items-center flex-col justify-center min-h-screen gap-2"
+      }
+    >
+      <Toaster />
+      <h1 className="text-3xl">what you seek is seeking you</h1>
+      <div ref={cookieRef} className="text-center text-xl">
+        <p>
+          This website uses cookies to provide necessary website functionality, improve your
+          experience and analyze our traffic.
+          <br />
+          By using our website, you agree to our Privacy Policy and our Cookies Policy.
+        </p>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 mr-2"
+          onClick={() => {
+            if (!player) return toast.error("Something went wrong. Try again.");
+            if (cookieRef.current && videoRef.current) {
+              cookieRef.current.style.display = "none";
+              videoRef.current.style.display = "block";
+            }
+            player.playVideo();
+          }}
+        >
+          ACCEPT
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          onClick={() => {
+            if (!player) return toast.error("Something went wrong. Try again.");
+            if (cookieRef.current && videoRef.current) {
+              cookieRef.current.style.display = "none";
+              videoRef.current.style.display = "block";
+            }
+            player.playVideo();
+          }}
+        >
+          DENY
+        </button>
+      </div>
+      <div ref={videoRef} style={{ display: "none" }}>
+        <YouTube
+          videoId={"fUy78IYJNMI"}
+          onReady={(e) => {
+            setPlayer(e.target);
+          }}
+        />
+        <h1>Happy Leeking ;)</h1>
+      </div>
     </main>
   );
 }
